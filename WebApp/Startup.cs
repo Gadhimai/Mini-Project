8277360ee69a66cb500b7736eedc8cf2a50b0d65
Gadhimai <saugatpanta3@gmail.com>
2022-08-03 10:42:00 +0545

    Work on Authentication and Authorization

--- WebApp/Startup.cs
+++ WebApp/Startup.cs
@@ -40,6 +40,12 @@ namespace WebApp
                 options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
             });
 
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy("AdminOnly", p => p.RequireClaim("Position", "Admin"));
+                options.AddPolicy("CashierOnly", p => p.RequireClaim("Position", "Cashier"));
+            });
+
             //Dependency Injection for In-Memory Data Store
             //services.AddScoped<ICategoryRepository, CategoryInMemoryRepository>();
             //services.AddScoped<IProductRepository, ProductInMemoryRepository>();
@@ -86,9 +92,12 @@ namespace WebApp
             app.UseStaticFiles();
 
             app.UseRouting();
+            app.UseAuthentication();
+            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapRazorPages();
                 endpoints.MapBlazorHub();
                 endpoints.MapFallbackToPage("/_Host");
             });
